8e90bae8311d13f81a83567331cd46d8ca0b8f49
kaichieh <kaichiehliao@gmail.com>
2026-02-24 01:11:51 +0800

    Add random number generation to demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
+import random
 print("Hi, I am Jay Liao from AIRT team. Thank you for reviewing this demo. When you see this message, it means the script has already been executed.")
-print(1+1)
+print("Random number:", random.randint(1, 100))
