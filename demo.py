6fb618b4b97b32a42c04cd177926df213d8499dd
kaichieh <kaichiehliao@gmail.com>
2026-02-24 01:45:03 +0800

    Update greeting message in demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
 import random
-print("Hi, I am Jay Liao from AIRT team. Thank you for reviewing this demo. When you see this message, it means the script has already been executed.")
+print("Hi, I’m Jay Liao from the AIRT team. Thank you for reviewing this demo.")
+print("When you see this message, it means the script has already been executed.")
 print("Random number:", random.randint(1, 100))
