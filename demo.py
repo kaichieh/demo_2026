da05df66465980a05686b4afb6719900c33f4d80
kaichieh <kaichiehliao@gmail.com>
2026-02-24 23:04:56 +0800

    Update execution message in demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 import random
 print("Hi, I’m Jay Liao from the AIRT team. Thank you for reviewing this demo.")
-print("When you see this message, it means the script has already been executed.")
+print("When you see a random number below, it means the script has already been executed.")
 print("Random number:", random.randint(1, 100))
